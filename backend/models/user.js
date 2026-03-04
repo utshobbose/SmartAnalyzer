73cffc941c39e830730cc6fbf89a7fcb9f7f541b
utshobbose <boseadidev1@gmail.com>
2026-03-04 17:45:37 +0600

    Upd

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,23 +2,23 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true, trim: true },
-    email: { type: String, required: true, unique: true, lowercase: true },
-    password: { type: String, required: true },
-    resumes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Resume" }],
-  },
-  { timestamps: true }
+    {
+        name: { type: String, required: true, trim: true },
+        email: { type: String, required: true, unique: true, lowercase: true },
+        password: { type: String, required: true },
+        resumes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Resume" }],
+    },
+    { timestamps: true }
 );
 
 userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
 });
 
 userSchema.methods.comparePassword = function (plain) {
-  return bcrypt.compare(plain, this.password);
+    return bcrypt.compare(plain, this.password);
 };
 
 module.exports = mongoose.model("User", userSchema);
